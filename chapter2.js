3340b8abe61a0bfb07b50814096865630a20b1da
Joeun <imjoeunha@gmail.com>
2017-02-20 21:22:51 +0900

    17.02.20
    - chapter1 & chapter2

--- chapter2.js
+++ chapter2.js
@@ -9,3 +9,28 @@
   - for
 4. const 키워드
 */
+
+(function() {
+  console.log("111");
+})()
+
+!function() {
+  console.log("2222");
+}()
+
+var func1 = function(a, b, c) {
+  console.log(a,b,c);
+  
+  arguments.forEach((v) => {
+   console.log(v);
+  })
+}
+
+var func2 = (a,b,c) => {
+  console.log(a,b,c);
+  console.log(arguments);
+}
+
+[1,2,3].forEach((v) => {
+  console.log(v);
+})
